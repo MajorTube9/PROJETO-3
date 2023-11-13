903290ab5f8dd88f798b68c8fed34831b12137fd
MajorTube9 <gabigo.machado@gmail.com>
2023-11-12 21:08:30 -0300

    INICIO
    Pegando o Projeto 1 para modificar

---
+++ biblioteca.h
@@ -0,0 +1,24 @@
+//
+// Created by Pedro Satoru on 12/09/2023.
+//
+
+#ifndef PROJETO1_BIBLIOTECA_H
+#define PROJETO1_BIBLIOTECA_H
+#define MAX_TAREFAS 100
+struct tarefa {
+    int prioridade;
+    char descricao[300];
+    char categoria[100];
+};
+
+void salvarTarefas(struct tarefa tarefas[], int numTarefas);
+
+int carregarTarefas(struct tarefa tarefas[]);
+
+void cadastrarTarefa(struct tarefa tarefas[], int *numTarefas);
+
+void listarTarefas(struct tarefa tarefas[], int numTarefas);
+
+void deletarTarefa(struct tarefa tarefas[], int *numTarefas, int prioridade, char categoria[]);
+
+#endif //PROJETO1_BIBLIOTECA_H
